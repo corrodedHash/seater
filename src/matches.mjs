b64b579432d3559455ae3cd7691022d7977b63d4
Lukas Wölfer <lukas.woelfer@rwth-aachen.de>
2025-03-13 15:00:24 +0100

    Worked on server matches api

--- src/matches.mjs
+++ src/matches.mjs
@@ -1,16 +1,24 @@
-import { RoomManagement } from "./services/room.mjs"
 import { UserManagement } from "./services/user.mjs"
 import express from 'express'
-/** @import {User, Room} from "./exchangeData" */
 
 /**
- * 
+ * @param {UserManagement} [users]
  */
 export function matches() {
     const app = express.Router()
 
     app.get("/", (req, res) => {
         res.json(res.locals.matches)
+    })
+
+    app.put('/', (req, res) => {
+        if (!res.locals.room.admins.includes(res.locals.user.id)) {
+            res.status(401)
+            res.end()
+            return
+        }
+        res.locals.matches = req.body
+        res.end()
     })
 
     return app
