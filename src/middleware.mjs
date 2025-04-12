9b1d42f1c5a868b844906754b137ad4d7acda944
Lukas Wölfer <lukas.woelfer@rwth-aachen.de>
2025-04-12 14:24:26 +0200

    Minor refactoring

--- src/middleware.mjs
+++ src/middleware.mjs
@@ -4,7 +4,9 @@ import { UserManagement } from "./services/user.mjs"
  * @param {UserManagement} users
  */
 export function userRoomAuth(users) {
-    return function userAuth(req, res, next) {
+    return function userAuth(
+        req, res,
+        /** @type {() => void} */ next) {
         const u = users.match(req)
         if (u === undefined) {
             res.status(401).end()
